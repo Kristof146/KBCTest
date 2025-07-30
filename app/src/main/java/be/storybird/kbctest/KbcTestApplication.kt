1a549b8b1e9df86db5da16c9f2f3a350a08eee9b
KristofVD <kristof.e.van.daele@gmail.com>
2025-07-30 20:36:26 +0200

    update dependencies and add hilt

---
+++ app/src/main/java/be/storybird/kbctest/KbcTestApplication.kt
@@ -0,0 +1,10 @@
+package be.storybird.kbctest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Kristof Van Daele.
+ */
+@HiltAndroidApp
+class KbcTestApplication : Application()
