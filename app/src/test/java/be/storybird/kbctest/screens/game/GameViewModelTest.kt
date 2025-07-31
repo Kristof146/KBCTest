2dcede68b20b1f53613aca15e6391454566f9aee
KristofVD <kristof.e.van.daele@gmail.com>
2025-07-31 09:47:45 +0200

    add more tests, last case is wrong

--- app/src/test/java/be/storybird/kbctest/screens/game/GameViewModelTest.kt
+++ app/src/test/java/be/storybird/kbctest/screens/game/GameViewModelTest.kt
@@ -43,9 +43,51 @@ class GameViewModelTest {
 		viewModel.onLetterUpdated(2, "C")
 		viewModel.onLetterUpdated(3, "D")
 		viewModel.onCheckCodeClicked()
-		val guess = viewModel.gameState.value.guess
 		val colors = viewModel.gameState.value.boxColors
-		assertEquals(listOf('A', 'B', 'C', 'D'), guess)
 		assertEquals(listOf(GameViewModel.colorGreen, GameViewModel.colorGreen, GameViewModel.colorGreen, GameViewModel.colorGreen), colors)
+	}
+
+	@Test
+	fun `updateGuess all wrong returns all red`(): Unit = runBlocking {
+		viewModel.onLetterUpdated(0, "E")
+		viewModel.onLetterUpdated(1, "F")
+		viewModel.onLetterUpdated(2, "G")
+		viewModel.onLetterUpdated(3, "H")
+		viewModel.onCheckCodeClicked()
+		val colors = viewModel.gameState.value.boxColors
+		assertEquals(listOf(GameViewModel.colorRed, GameViewModel.colorRed, GameViewModel.colorRed, GameViewModel.colorRed), colors)
+	}
+
+	@Test
+	fun `updateGuess all correct but wrong location returns all orange`(): Unit = runBlocking {
+		viewModel.onLetterUpdated(0, "B")
+		viewModel.onLetterUpdated(1, "C")
+		viewModel.onLetterUpdated(2, "D")
+		viewModel.onLetterUpdated(3, "A")
+		viewModel.onCheckCodeClicked()
+		val colors = viewModel.gameState.value.boxColors
+		assertEquals(listOf(GameViewModel.colorOrange, GameViewModel.colorOrange, GameViewModel.colorOrange, GameViewModel.colorOrange), colors)
+	}
+
+	@Test
+	fun `updateGuess mix colors`(): Unit = runBlocking {
+		viewModel.onLetterUpdated(0, "A")
+		viewModel.onLetterUpdated(1, "C")
+		viewModel.onLetterUpdated(2, "F")
+		viewModel.onLetterUpdated(3, "D")
+		viewModel.onCheckCodeClicked()
+		val colors = viewModel.gameState.value.boxColors
+		assertEquals(listOf(GameViewModel.colorGreen, GameViewModel.colorOrange, GameViewModel.colorRed, GameViewModel.colorGreen), colors)
+	}
+
+	@Test
+	fun `updateGuess duplicate letter`(): Unit = runBlocking {
+		viewModel.onLetterUpdated(0, "A")
+		viewModel.onLetterUpdated(1, "A")
+		viewModel.onLetterUpdated(2, "F")
+		viewModel.onLetterUpdated(3, "G")
+		viewModel.onCheckCodeClicked()
+		val colors = viewModel.gameState.value.boxColors
+		assertEquals(listOf(GameViewModel.colorGreen, GameViewModel.colorRed, GameViewModel.colorRed, GameViewModel.colorRed), colors)
 	}
 }
